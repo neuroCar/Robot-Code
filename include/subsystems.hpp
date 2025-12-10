d5588fe7950342cf3d2f5296ec3316d87b5dee82
Natu1108 <n2194050179@gmail.com>
2025-12-10 16:25:39 -0600

    Code fur die Robot

---
+++ include/subsystems.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+
+extern Drive chassis;
+
+// Your motors, sensors, etc. should go here.  Below are examples
+
+inline pros::Motor intake(7);
+inline pros::Motor top(-19);
+inline ez::Piston descore('H');
+
+// inline pros::Motor intake(1);
+// inline pros::adi::DigitalIn limit_switch('A');
