be87aff575a4d5de8678620a0a706ee1f8390576
Neuro Car <n2194050179@gmail.com>
2026-01-09 15:16:57 -0600

    Edit autos descriptions

--- src/main.cpp
+++ src/main.cpp
@@ -59,7 +59,7 @@ void initialize() {
   // Autonomous Selector using LLEMU
   ez::as::auton_selector.autons_add({
       {"Skills\n\nSkills Autos", drive_example},
-      {"Turn\n\nTurn 3 times.", turn_example},
+      {"Match\n\nMatch Autos.", turn_example},
       {"Drive and Turn\n\nDrive forward, turn, come back", drive_and_turn},
       {"Drive and Turn\n\nSlow down during drive", wait_until_change_speed},
       {"Swing Turn\n\nSwing in an 'S' curve", swing_example},
