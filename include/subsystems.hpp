139b6c7bb99a0c6a24267431e0e66a32f0b393a4
Neuro Car <n2194050179@gmail.com>
2026-01-18 20:24:36 -0600

    Update ports for rebuild

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -7,10 +7,7 @@ extern Drive chassis;
 
 // Your motors, sensors, etc. should go here.  Below are examples
 
-inline pros::Motor intake(-11);
-inline pros::Motor top(16);
-inline ez::Piston descore('H');
-inline ez::Piston matchLoader('G');
-
-// inline pros::Motor intake(1);
-// inline pros::adi::DigitalIn limit_switch('A');
+inline pros::Motor intake(-10);
+inline pros::Motor top(19);
+inline ez::Piston descore('A');
+inline ez::Piston matchLoader('B');
--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,10 @@
 // Chassis constructor
 ez::Drive chassis(
     // These are your drive motors, the first motor is used for sensing!
-    {-1, -2, -3},     // Left Chassis Ports (negative port will reverse it!)
-    {9, 7, 8},  // Right Chassis Ports (negative port will reverse it!)
+    {-2, -1, -13},     // Left Chassis Ports (negative port will reverse it!)
+    {20, 14, 4},  // Right Chassis Ports (negative port will reverse it!)
 
-    10,      // IMU Port
+    5,      // IMU Port
     3.25,  // Wheel Diameter (Remember, 4" wheels without screw holes are actually 4.125!)
     360);   // Wheel RPM = cartridge * (motor gear / wheel gear)
 
